5a1c7952242aa75e154a752a27768cdfbe99e97d
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-06 21:21:56 -0500

    organizado clases

---
+++ clases/Ent.js
@@ -0,0 +1,9 @@
+class Ent {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    draw() {
+        console.log("test");
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+test = new Ent
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  test.draw();
 }
