3d4b316cfb980e6c7893adf8f0515489d34c568a
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-12 10:33:14 -0500

    se arregla el autocompletado

--- clases/Ent.js
+++ clases/Ent.js
@@ -3,15 +3,15 @@ class Ent {
         this.x = x;
         this.y = y;
         this.tipo = "nada";
-        this.ancho = 0;
-        this.alto = 0;
+        this.ancho = 40;
+        this.alto = 40;
         this.estado = 0;
         this.valor = 0;
     }
     draw() {
         console.log("test");
         fill(255);
-        rect(this.x,this.y,30,30);
+        rect(this.x,this.y,this.ancho,this.alto);
     }
     parametros() {
 
