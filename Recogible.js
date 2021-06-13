8543567fb10abdad731123c14b53a39a4c8d5f4e
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 10:44:17 -0500

    base de Recogible

--- Recogible.js
+++ Recogible.js
@@ -1,1 +1,17 @@
 // una clase padre que contiene a los entes que pueden ser coleccionados por el jugador
+
+class Recogible extends Ent {
+    constructor(x, y) {
+        super(x, y);
+        // determina si fue recogido
+        this.recogido = false;
+    }
+
+    // cambia el estado de recogido
+
+    recoger() {
+        if (this.tocado() === "jugador") {
+            this.recogido = true;
+        }
+    }
+}
