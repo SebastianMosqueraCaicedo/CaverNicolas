9b278ac3ab1f51b5408c0ec3b4aba5219adba659
Lachosk <1006009500@u.icesi.edu.co>
2021-06-13 16:12:05 -0500

    las imagenes

--- sketch.js
+++ sketch.js
@@ -1,18 +1,32 @@
 // Imagenes
 // bichos
-let imgabeja;
+let imgabeja; 
 let imgcentinela;
 let imggusanorojo;
 let imgusanoAzul;
+let imgcreeper;
+let imghormiganormal
+let imghormigaverde
+let imgmurcielago
+let imgserpientenormal
+let imgserpienteroja
+
+
+
 
 function preload() {
-
   // bichos
   imgcentinela = loadImage('data/bichos/centinela.png');
   imgabeja = loadImage('data/bichos/abeja.png');
   imggusanorojo = loadImage('data/bichos/gusano_rojo.png');
   imgusanoAzul = loadImage ('data/bichos/gusano_azul.png');
-
+  imgcreeper = loadImage ('data/bichos/creeper.png');
+  imghormiganormal = loadImage ('data/bichos/hormiga_normal.png');
+  imghormigaverde = loadImage ('data/bichos/hormiga_verde.png');
+  imgmurcielago = loadImage ('data/bichos/murcielago.png');
+  imgserpientenormal = loadImage ('data/bichos/serpiente_normal.png');
+  imgserpienteroja = loadImage ('data/bichos/serpiente_roja.png');
+  
 }
 
 
