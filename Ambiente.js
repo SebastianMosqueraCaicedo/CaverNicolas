c4360add2098c2bf2d861d1b15aa1306098a0e0c
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 11:02:09 -0500

    se agrega generalidad a Ambiente

--- Ambiente.js
+++ Ambiente.js
@@ -9,20 +9,22 @@ class Ambiente extends Ent {
 
     tocado(ent) {
         // se agrega la variable de distancia, debido a que los ambientes siempre ocuparan 40x40
-        if (this.getSup() > ent.getSup() &&
-            this.getSup() < ent.getInf() &&
-            this.getX() > ent.getIzq() &&
-            this.getX() < ent.getDer() || this.getInf() > ent.getSup() &&
-            this.getInf() < ent.getInf() &&
-            this.getX() > ent.getIzq() &&
-            this.getX() < ent.getDer() || this.getDer() > ent.getIzq() &&
-            this.getDer() < ent.getDer() &&
-            this.getY() > ent.getSup() &&
-            this.getY() < ent.getInf() || this.getIzq() > ent.getIzq() &&
-            this.getIzq() < ent.getDer() &&
-            this.getY() > ent.getSup() &&
-            this.getY() < ent.getInf() || dist(this.getX(), this.getY(), ent.getX(), ent.getY()) < 20) {
-            return ent.tipo;
+
+        if ((this.getSup() > ent.getSup() &&
+                this.getSup() < ent.getInf() &&
+                this.getX() > ent.getIzq() &&
+                this.getX() < ent.getDer() || this.getInf() > ent.getSup() &&
+                this.getInf() < ent.getInf() &&
+                this.getX() > ent.getIzq() &&
+                this.getX() < ent.getDer() || this.getDer() > ent.getIzq() &&
+                this.getDer() < ent.getDer() &&
+                this.getY() > ent.getSup() &&
+                this.getY() < ent.getInf() || this.getIzq() > ent.getIzq() &&
+                this.getIzq() < ent.getDer() &&
+                this.getY() > ent.getSup() &&
+                this.getY() < ent.getInf() || dist(this.getX(), this.getY(), ent.getX(), ent.getY()) < 20) && this.caminable === false) {
+            ent.vel = -ent.vel;
+            // si el obstaculo no es caminable y es tocado por un ente, lo envia a la direccion opuesta
         } else {
             return null;
         }
