282ed842f5d36289d0a448852f773de94fc97c99
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 15:50:24 -0500

    corrigiendo errores

--- Abismo.js
+++ Abismo.js
@@ -1,6 +1,6 @@
 // un ambiente por donde no se puede caminar pero los proyectiles y entes voladores pueden pasar por ello. Si el personaje esta en la misma posicion de un abismo, muere
 
-class Abismo extends Abismo {
+class Abismo extends Ambiente {
     constructor(x, y) {
         super(x, y);
     }
