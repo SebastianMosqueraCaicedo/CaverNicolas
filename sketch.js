4fb262d9712bc6eb1d72c952957acd6705c31438
Miosekai <1010070539@u.icesi.edu.co>
2021-06-13 16:52:57 -0500

    nicolas y objetos

--- sketch.js
+++ sketch.js
@@ -18,8 +18,30 @@ let imgactorderecha;
 
 
 //nicolas
+let imgnicolasatras;
+let imgnicolasderecha;
+let imgnicolasderechamano;
+let imgnicolasfrente;
+let imgnicolasizquierda;
+let imgnicolasizquierdamano;
+let imagnicolasmuerte;
+let imagnicolasnormal;
 
 //objetos
+let imgbarril;
+let imgcarro;
+let imgcorazon0vida;
+let imgcorazonmediavida;
+let imgcorazontodavida;
+let imgcuevaabierta;
+let imgcuevacerrada;
+let imginterfaz;
+let imglatigo;
+let imgpistola;
+let imgpocima;
+let imgpuertaabierta;
+let imgpuertacerrada;
+let imgsimioazul;
 
 
 
@@ -45,8 +67,34 @@ function preload() {
   //humo
 
   //nicolas
+  imgnicolasatras = loadImage('data/nicolas/nicolas_atras.png');
+  imgnicolasderecha = loadImage ('data/nicolas/nicolas_derecha.png');
+  imgnicolasderechamano = loadImage ('data/nicolas/nicolas_derecha_mano.png');
+  imgnicolasfrente = loadImage ('data/nicolas/nicolas_frente.png');
+  imgnicolasizquierda = loadImage ('data/nicolas/nicolas_izquierda.png');
+  imgnicolasizquierdamano = loadImage ('data/nicolas/nicolas_izquierda_mano.png');
+  imagnicolasmuerte = loadImage ('data/nicolas/nicolas_muerte.png');
+  imagnicolasnormal = loadImage ('data/nicolas/nicolas_normal.png');
+
+
 
   //objetos
+  imgbarril = loadImage('data/objetos/barril.png');
+  imgcarro  = loadImage ('data/objetos/carro.png');
+  imgcorazon0vida = loadImage ('data/objetos/corazon_0vida.png');
+  imgcorazonmediavida = loadImage ('data/objetos/corazon_media_vida.png');
+  imgcorazontodavida = loadImage ('data/objetos/corazon_toda_vida.png');
+  imgcuevaabierta = loadImage ('data/objetos/cueva_abierta.png');
+  imgcuevacerrada = loadImage ('data/objetos/cueva_cerrada.png');
+  imginterfaz = loadImage ('data/objetos/Interfaz.png');
+  imglatigo = loadImage ('data/objetos/latigo.png');
+  imgpistola = loadImage ('data/objetos/pistola.png');
+  imgpocima = loadImage ('data/objetos/pocima.png');
+  imgpuertaabierta = loadImage ('data/objetos/puerta_abierta.png');
+  imgpuertacerrada = loadImage ('data/objetos/puerta_cerrada.png');
+  imgsimioazul = loadImage ('data/objetos/simio_azul.png');
+
+
 
 }
 
