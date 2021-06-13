80a4c1c5688125bc5dcd3811d234ae34ac07777e
Lachosk <1006009500@u.icesi.edu.co>
2021-06-13 16:53:50 -0500

    Imgs

--- sketch.js
+++ sketch.js
@@ -13,15 +13,53 @@ let imgserpienteroja
 
 //Jonh
 let imgactorderecha;
-//fondos
-//humo
+let imgactorizquierda;
+let imgactorfrente;
+let imgactorartras;
 
+let imgactorderechapistola;
+let imgactorizquierdapistola;
+let imgactorfrentepistola;
+let imgactorartraspistola;
+
+let imgactorderechalatigo;
+let imgactorizquierdalatigo;
+
+//fondos
+let imgfondotest;
+let imgpantalla4;
+let imgpantalla5;
+let imgpantalla6;
+let imgpantalla7;
+let imgpantalla8;
+let imgpantalla9;
+let imgpantalla10;
+let imgpantalla11;
+let imgpantalla12;
+let imgpantalla13;
+let imgpantalla14;
+let imgpantalla15;
+
+//humo
+let imghumo1_1;
+let imghumo1_2;
+let imghumo1_3;
+
+let imghumo2_1;
+let imghumo2_2;
+let imghumo2_3;
 
 //nicolas
 
 //objetos
 
 
+
+//pantallas
+let imggameover;
+let imginicio;
+let imgistrucciones;
+let imgvictoria;
 
 
 
@@ -39,15 +77,59 @@ function preload() {
   imgserpientenormal = loadImage ('data/bichos/serpiente_normal.png');
   imgserpienteroja = loadImage ('data/bichos/serpiente_roja.png');
   imgusanoAzul = loadImage('data/bichos/gusano_azul.png');
-  imgactorderecha = loadImage('data/actor/actor_derecha.png');
+  
   // John
+  imgactorderecha = loadImage('data/actor/actor_derecha.png');
+  imgactorizquierda = loadImage('data/actor/actor_izquierda.png');
+  imgactorfrente = loadImage('data/actor/actor_derecha.png');
+  imgactorartras  = loadImage('data/actor/actor_atras.png');
+
+  imgactorderechapistola = loadImage('data/actor/actor_derecha_pistola.png');
+  imgactorizquierdapistola = loadImage('data/actor/actor_izquierda_pistola.png');
+  imgactorfrentepistola = loadImage('data/actor/actor_derecha_pistola.png');
+  imgactorartraspistola  = loadImage('data/actor/actor_atras_pistola.png');
+
+  imgactorderechalatigo = loadImage('data/actor/actor_derecha_pistola.png');
+  imgactorizquierdalatigo = loadImage('data/actor/actor_izquierda_pistola.png');
+  
   //fondos
+  imgfondotest = loadImage('data/fondos/Fondo_Test.png');
+  imgpantalla4 = loadImage('data/fondos/pantalla_4.png');
+  imgpantalla5 = loadImage('data/fondos/pantalla_5.png');
+  imgpantalla6 = loadImage('data/fondos/pantalla_6.png');
+  imgpantalla7 = loadImage('data/fondos/pantalla_7.png');
+  imgpantalla8 = loadImage('data/fondos/pantalla_8.png');
+  imgpantalla9 = loadImage('data/fondos/pantalla_9.png');
+  imgpantalla10 = loadImage('data/fondos/pantalla_10.png');
+  imgpantalla11 = loadImage('data/fondos/pantalla_11.png');
+  imgpantalla12 = loadImage('data/fondos/pantalla_12.png');
+  imgpantalla13 = loadImage('data/fondos/pantalla_13.png');
+  imgpantalla14 = loadImage('data/fondos/pantalla_14.png');
+  imgpantalla15 = loadImage('data/fondos/pantalla_15.png');
+
   //humo
+  imghumo1_1 = loadImage('data/humo/humo_f_1.png');
+  imghumo1_2 = loadImage('data/humo/humo_f_2.png');
+  imghumo1_3 = loadImage('data/humo/humo_f_3.png');
+
+  imghumo2_1 = loadImage('data/humo/humo2_f_1.png');
+  imghumo2_2 = loadImage('data/humo/humo2_f_1.png');
+  imghumo2_3 = loadImage('data/humo/humo2_f_1.png');
 
   //nicolas
 
   //objetos
 
+
+
+
+  //pantallas
+  imggameover = loadImage('data/pantallas/gameover.png');
+  imginicio = loadImage('data/pantallas/inicio.png');
+  imgistrucciones = loadImage('data/pantallas/insturcciones.png');
+  imgvictoria = loadImage('data/pantallas/victoria.png');
+
+  
 }
 
 
