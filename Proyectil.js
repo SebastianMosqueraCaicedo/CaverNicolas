e575291ebe067819ee1654654ff8663a6eb8b631
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 12:26:34 -0500

    se crea la aceleracion del proyectil

--- Proyectil.js
+++ Proyectil.js
@@ -1,10 +1,12 @@
 // una clase padre conteniendo a los ataques a distancia (latigo, acido, bala, roca)
 
-class Proyectil extends Actor { 
+class Proyectil extends Actor {
     constructor(x, y) {
         super(x, y);
-       
+        // determina cual es la distancia maxima que recorrera el proyectil    
+        this.distanciaTotal = 1;
+        // reemplaza a velocidad y cambia dependiendo de esta y la distancia recorrida
+        this.accel = this.vel - (this.vel / (this.distanciaTotal - (this.distancia - 1)));
 
     }
 }
-
