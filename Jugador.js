cda77e5c92a99bee913ff1e6c9501a0b8443675b
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 13:19:19 -0500

    generalidades de interfaz en jugador

--- Jugador.js
+++ Jugador.js
@@ -3,5 +3,48 @@
 class Jugador extends Actor {
     constructor(x, y) {
         super(x, y);
+        // determina si puede usar la pistola
+        // 0 no tiene, 1 no puede usarla, 2 puede usarla , 3 puede usarla y esta mejorada
+        this.estadoPistola = 0;
+        // determina si puede usar el latigo
+        // 0 no tiene, 1 no puede usarlo, 2 puede usarlo , 3 puede usarlo y esta mejorado
+        this.estadoLatigo = 0;
+        // cuenta la cantidad de carriles
+        this.cantidadCarriles = 0;
+        // cuenta la cantidad de pocimas
+        this.cantidadPocimas = 0;
+        // cuenta la cantidad de Llaves
+        this.cantidadLlaves = 0;
     }
+
+    // devuelve la cantidad de monedas que tiene el jugador
+
+    getMonedas() {
+        return this.monedas;
+    }
+
+    // devuelve la vida actual del jugador
+
+    getVida() {
+        return this.vida;
+    }
+
+    // devuelve los carriles
+
+    getCarriles() {
+
+    }
+
+    // devuelve las pocimas
+
+    getPocimas() {
+
+    }
+
+    // devuelve las Llaves
+
+    getLlaves() {
+
+    }
+
 }
