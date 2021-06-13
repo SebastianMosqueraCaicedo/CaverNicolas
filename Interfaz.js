ff47004b841fb6e86274c65063ec84b26a747f5a
Lachosk <1006009500@u.icesi.edu.co>
2021-06-13 18:07:50 -0500

    interfaz

--- Interfaz.js
+++ Interfaz.js
@@ -1,20 +1,32 @@
 class Interfaz extends Ent {
     constructor(x, y) {
         super(x, y);
+        this.contaMonedas = 0;
+        this.contaPociones = 0;
+        this.contaCarriles = 0;
+        this.contaLlaves = 0;
+        this.puntaje = 0;
+        this.contaVida = 0;
     }
 
-draw(){
-    switch (this.estado) {
-        case 9:
-            
-            break;
+
+
+
+draw(){   
+if (this.estado === 1) {
+    image(imginterfaz, this.x, this.y, this.ancho, this.alto);
+
     
-        default:
-            break;
-    }
+}   
+
 }
 
     contar(jugador){
-
+        this.contaMonedas = jugador.getMonedas();
+        this.contaPociones = jugador.getPocimas();
+        this.contaCarriles = jugador.getCarriles();
+        this.contaLlaves = jugador.getLlaves();
+        this.contaVida = jugador.getVida();
+        
     }
 }
