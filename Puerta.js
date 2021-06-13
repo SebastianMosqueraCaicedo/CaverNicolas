ecc0434741ca03c7a745e9554a26876e245ac406
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 18:05:45 -0500

    base de puerta

--- Puerta.js
+++ Puerta.js
@@ -1,1 +1,8 @@
 // un ambiente que cambia el estado de la pantalla
+
+class Puerta extends Ambiente {
+    constructor(x, y, identificador) {
+        super(x, y);
+        this.identificador = identificador;
+    }
+}
