ce11ae07f29f2d2a6141c0dda619931884d01784
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 17:41:39 -0500

    base interfaz 2

--- Interfaz.js
+++ Interfaz.js
@@ -2,4 +2,19 @@ class Interfaz extends Ent {
     constructor(x, y) {
         super(x, y);
     }
+
+draw(){
+    switch (this.estado) {
+        case 9:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+    contar(jugador){
+
+    }
 }
