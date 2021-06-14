fa2b4337ba1a74d3216039015c2b9a4c7d1f8443
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-06-13 19:09:38 -0500

    base pantalla

--- Pantalla.js
+++ Pantalla.js
@@ -1,1 +1,71 @@
 // contiene la informacion de donde van a ir los ambientes y la imagen de fondo, dependiendo del estado
+
+class Pantalla extends Ent {
+    constructor(x, y) {
+        super(x, y);
+    }
+
+    draw() {
+        super.parametros();
+        switch (this.estado) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+            case 14:
+
+                break;
+            case 15:
+
+                break;
+            case 20:
+
+                break;
+            case 21:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    cambiarPantalla(puerta) {
+
+    }
+}
